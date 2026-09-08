bc6c3416ee6d9126499669f9e88619145bcda96a
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-09-08 22:13:59 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import express from "express";
-const app = express();
+import cors from "cors";
+
 import authRouter from "./routes/auth.route.js";
 
+const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/api/v1/auth", authRouter);
+
+
 app.get("/health", (req, res) => {
     res.status(200).json({
         success: true,
